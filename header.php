2deb76489fefaa0bbace7d62881d5577162c15dd
Andros Fenollosa <andros@fenollosa.email>
2020-03-12 12:52:15 +0100

    Fist commit

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<!-- Título que aparecerá en la pestaña - La estructura será el nombre de la página actual + Descripción del sitio -->
+		<title>Inicio</title>
+		<!-- Icono que acompañará al title. También será el icono que se guardará a la hora de que hagan un marcador. Se recomienda un tamaño de 32x32 y en PNG	-->
+		<link rel="icon" type="image/png" href="<?php bloginfo( 'template_url' ); ?>/img/favicon.png">
+		<!-- (Opcional) Sugiere un color al navegador para personalizar la ventana -->
+		<meta name="theme-color" content="#3c790a">
+		<!-- Bloquea el zoom en smartphones-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
+		<!--Declara quien es el autor. Sin utilidad práctica -->
+		<meta name="author" content="Pixelmouse">
+		<!-- Declara la descripción de la web. Es utilizado por los motores de busqueda para la previa -->
+		<meta name="description" content="Descripcion de tu web">
+		<!-- Open Graph y Twitter card: Declara como se mostrará la previa del enlace si es compartida por Facebook o alguna otra red social -->
+		<meta property="og:image" content="<?php bloginfo( 'template_url' ); ?>/img/miniatura.png">
+		<meta property="og:title" content="The Rock">
+		<!-- website, video, audio, article, book o profile -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="http://www.imdb.com/title/tt0117500/">
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@cuenta">
+		<meta name="twitter:creator" content="@cuenta">
+		<!-- Open Graph Imagen completa (Opcional) -->
+		<meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg">
+		<meta property="og:image:type" content="image/jpeg">
+		<meta property="og:image:width" content="400">
+		<meta property="og:image:height" content="300">
+		<meta property="og:image:alt" content="A shiny red apple with a bite taken out">
+
+		<!-- CSS -->
+		<link href="<?php bloginfo( 'template_url' ); ?>/css/mobile.css" rel="stylesheet" media="all and (max-width: 600px)">
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/desktop.css" rel="stylesheet" media="all and (min-width: 600px)">
+		<!-- End CSS -->
+
+		<?php wp_head(); ?>
+
+	</head>
+
+	<body <?php body_class(); ?>>
